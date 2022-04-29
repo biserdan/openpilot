1770b5ef9e56338d6df753c979b309fe700fef37
Calvin1858 <kevin_steiger@gmx.ch>
2022-04-29 13:53:43 +0200

    switch camera id's and enable SIMULATION

--- selfdrive/camerad/cameras/camera_webcam.cc
+++ selfdrive/camerad/cameras/camera_webcam.cc
@@ -19,8 +19,8 @@
 #include "selfdrive/common/util.h"
 
 // id of the video capturing device
-const int ROAD_CAMERA_ID = util::getenv("ROADCAM_ID", 1);
-const int DRIVER_CAMERA_ID = util::getenv("DRIVERCAM_ID", 0);
+const int ROAD_CAMERA_ID = util::getenv("ROADCAM_ID", 0);
+const int DRIVER_CAMERA_ID = util::getenv("DRIVERCAM_ID", 1);
 
 #define FRAME_WIDTH  1164
 #define FRAME_HEIGHT 874
