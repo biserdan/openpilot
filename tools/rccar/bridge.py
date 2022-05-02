a6c4c1e5f0e31fbe8e3c70607a33009cd5969d11
Calvin1858 <kevin_steiger@gmx.ch>
2022-05-02 18:33:01 +0200

    add communication over socket

--- tools/rccar/bridge.py
+++ tools/rccar/bridge.py
@@ -6,6 +6,7 @@ import time
 import os
 from multiprocessing import Process, Queue
 from typing import Any
+import socket
 
 import numpy as np
 # import pyopencl as cl
@@ -32,6 +33,9 @@ W, H = 1928, 1208
 REPEAT_COUNTER = 5
 PRINT_DECIMATION = 100
 STEER_RATIO = 15.
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 3333
 
 # pm = messaging.PubMaster(['roadCameraState', 'sensorEvents', 'can', "gpsLocationExternal"])
 pm = messaging.PubMaster(['roadCameraState', 'can'])
@@ -215,240 +219,245 @@ def can_function_runner(vs: VehicleState, exit_event: threading.Event):
 
 
 def bridge(q):
-  # # setup CARLA
-  # client = carla.Client("127.0.0.1", 2000)
-  # client.set_timeout(10.0)
-  # world = client.load_world(args.town)
-  #
-  # settings = world.get_settings()
-  # settings.synchronous_mode = True # Enables synchronous mode
-  # settings.fixed_delta_seconds = 0.05
-  # world.apply_settings(settings)
-  #
-  # world.set_weather(carla.WeatherParameters.ClearSunset)
-  #
-  # if args.low_quality:
-  #   world.unload_map_layer(carla.MapLayer.Foliage)
-  #   world.unload_map_layer(carla.MapLayer.Buildings)
-  #   world.unload_map_layer(carla.MapLayer.ParkedVehicles)
-  #   world.unload_map_layer(carla.MapLayer.Props)
-  #   world.unload_map_layer(carla.MapLayer.StreetLights)
-  #   world.unload_map_layer(carla.MapLayer.Particles)
-  #
-  # blueprint_library = world.get_blueprint_library()
-  #
-  # world_map = world.get_map()
-  #
-  # vehicle_bp = blueprint_library.filter('vehicle.tesla.*')[1]
-  # spawn_points = world_map.get_spawn_points()
-  # assert len(spawn_points) > args.num_selected_spawn_point, \
-  #   f'''No spawn point {args.num_selected_spawn_point}, try a value between 0 and
-  #   {len(spawn_points)} for this town.'''
-  # spawn_point = spawn_points[args.num_selected_spawn_point]
-  # vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+  with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:  # UDP
+    # # setup CARLA
+    # client = carla.Client("127.0.0.1", 2000)
+    # client.set_timeout(10.0)
+    # world = client.load_world(args.town)
+    #
+    # settings = world.get_settings()
+    # settings.synchronous_mode = True # Enables synchronous mode
+    # settings.fixed_delta_seconds = 0.05
+    # world.apply_settings(settings)
+    #
+    # world.set_weather(carla.WeatherParameters.ClearSunset)
+    #
+    # if args.low_quality:
+    #   world.unload_map_layer(carla.MapLayer.Foliage)
+    #   world.unload_map_layer(carla.MapLayer.Buildings)
+    #   world.unload_map_layer(carla.MapLayer.ParkedVehicles)
+    #   world.unload_map_layer(carla.MapLayer.Props)
+    #   world.unload_map_layer(carla.MapLayer.StreetLights)
+    #   world.unload_map_layer(carla.MapLayer.Particles)
+    #
+    # blueprint_library = world.get_blueprint_library()
+    #
+    # world_map = world.get_map()
+    #
+    # vehicle_bp = blueprint_library.filter('vehicle.tesla.*')[1]
+    # spawn_points = world_map.get_spawn_points()
+    # assert len(spawn_points) > args.num_selected_spawn_point, \
+    #   f'''No spawn point {args.num_selected_spawn_point}, try a value between 0 and
+    #   {len(spawn_points)} for this town.'''
+    # spawn_point = spawn_points[args.num_selected_spawn_point]
+    # vehicle = world.spawn_actor(vehicle_bp, spawn_point)
 
-  # max_steer_angle = vehicle.get_physics_control().wheels[0].max_steer_angle
+    # max_steer_angle = vehicle.get_physics_control().wheels[0].max_steer_angle
 
-  # TODO
-  max_steer_angle = 70.0 # from carla doc
+    # TODO
+    max_steer_angle = 70.0 # from carla doc
 
-  # # make tires less slippery
-  # # wheel_control = carla.WheelPhysicsControl(tire_friction=5)
-  # physics_control = vehicle.get_physics_control()
-  # physics_control.mass = 2326
-  # # physics_control.wheels = [wheel_control]*4
-  # physics_control.torque_curve = [[20.0, 500.0], [5000.0, 500.0]]
-  # physics_control.gear_switch_time = 0.0
-  # vehicle.apply_physics_control(physics_control)
-  #
-  # blueprint = blueprint_library.find('sensor.camera.rgb')
-  # blueprint.set_attribute('image_size_x', str(W))
-  # blueprint.set_attribute('image_size_y', str(H))
-  # blueprint.set_attribute('fov', '40')
-  # blueprint.set_attribute('sensor_tick', '0.05')
-  # transform = carla.Transform(carla.Location(x=0.8, z=1.13))
-  # camera = world.spawn_actor(blueprint, transform, attach_to=vehicle)
-  # camerad = Camerad()
-  # camera.listen(camerad.cam_callback)
+    # # make tires less slippery
+    # # wheel_control = carla.WheelPhysicsControl(tire_friction=5)
+    # physics_control = vehicle.get_physics_control()
+    # physics_control.mass = 2326
+    # # physics_control.wheels = [wheel_control]*4
+    # physics_control.torque_curve = [[20.0, 500.0], [5000.0, 500.0]]
+    # physics_control.gear_switch_time = 0.0
+    # vehicle.apply_physics_control(physics_control)
+    #
+    # blueprint = blueprint_library.find('sensor.camera.rgb')
+    # blueprint.set_attribute('image_size_x', str(W))
+    # blueprint.set_attribute('image_size_y', str(H))
+    # blueprint.set_attribute('fov', '40')
+    # blueprint.set_attribute('sensor_tick', '0.05')
+    # transform = carla.Transform(carla.Location(x=0.8, z=1.13))
+    # camera = world.spawn_actor(blueprint, transform, attach_to=vehicle)
+    # camerad = Camerad()
+    # camera.listen(camerad.cam_callback)
 
-  vehicle_state = VehicleState()
+    vehicle_state = VehicleState()
 
-  # reenable IMU
-  # imu_bp = blueprint_library.find('sensor.other.imu')
-  # imu = world.spawn_actor(imu_bp, transform, attach_to=vehicle)
-  # imu.listen(lambda imu: imu_callback(imu, vehicle_state))
-  #
-  # gps_bp = blueprint_library.find('sensor.other.gnss')
-  # gps = world.spawn_actor(gps_bp, transform, attach_to=vehicle)
-  # gps.listen(lambda gps: gps_callback(gps, vehicle_state))
+    # reenable IMU
+    # imu_bp = blueprint_library.find('sensor.other.imu')
+    # imu = world.spawn_actor(imu_bp, transform, attach_to=vehicle)
+    # imu.listen(lambda imu: imu_callback(imu, vehicle_state))
+    #
+    # gps_bp = blueprint_library.find('sensor.other.gnss')
+    # gps = world.spawn_actor(gps_bp, transform, attach_to=vehicle)
+    # gps.listen(lambda gps: gps_callback(gps, vehicle_state))
 
-  # launch fake car threads
-  threads = []
-  exit_event = threading.Event()
-  threads.append(threading.Thread(target=panda_state_function, args=(vehicle_state, exit_event,)))
-  threads.append(threading.Thread(target=peripheral_state_function, args=(exit_event,)))
-  threads.append(threading.Thread(target=fake_driver_monitoring, args=(exit_event,)))
-  threads.append(threading.Thread(target=can_function_runner, args=(vehicle_state, exit_event,)))
-  for t in threads:
-    t.start()
+    # launch fake car threads
+    threads = []
+    exit_event = threading.Event()
+    threads.append(threading.Thread(target=panda_state_function, args=(vehicle_state, exit_event,)))
+    threads.append(threading.Thread(target=peripheral_state_function, args=(exit_event,)))
+    threads.append(threading.Thread(target=fake_driver_monitoring, args=(exit_event,)))
+    threads.append(threading.Thread(target=can_function_runner, args=(vehicle_state, exit_event,)))
+    for t in threads:
+      t.start()
 
-  # can loop
-  rk = Ratekeeper(100, print_delay_threshold=0.05)
+    # can loop
+    rk = Ratekeeper(100, print_delay_threshold=0.05)
 
-  # init
-  throttle_ease_out_counter = REPEAT_COUNTER
-  brake_ease_out_counter = REPEAT_COUNTER
-  steer_ease_out_counter = REPEAT_COUNTER
+    # init
+    throttle_ease_out_counter = REPEAT_COUNTER
+    brake_ease_out_counter = REPEAT_COUNTER
+    steer_ease_out_counter = REPEAT_COUNTER
 
-  # vc = carla.VehicleControl(throttle=0, steer=0, brake=0, reverse=False)
+    # vc = carla.VehicleControl(throttle=0, steer=0, brake=0, reverse=False)
 
-  is_openpilot_engaged = False
-  throttle_out = steer_out = brake_out = 0
-  throttle_op = steer_op = brake_op = 0
-  throttle_manual = steer_manual = brake_manual = 0
-
-  old_steer = old_brake = old_throttle = 0
-  throttle_manual_multiplier = 0.7  # keyboard signal is always 1
-  brake_manual_multiplier = 0.7  # keyboard signal is always 1
-  steer_manual_multiplier = 45 * STEER_RATIO  # keyboard signal is always 1
-
-  while True:
-    # 1. Read the throttle, steer and brake from op or manual controls
-    # 2. Set instructions in Carla
-    # 3. Send current carstate to op via can
-
-    cruise_button = 0
-    throttle_out = steer_out = brake_out = 0.0
+    is_openpilot_engaged = False
+    throttle_out = steer_out = brake_out = 0
     throttle_op = steer_op = brake_op = 0
-    throttle_manual = steer_manual = brake_manual = 0.0
+    throttle_manual = steer_manual = brake_manual = 0
 
-    # --------------Step 1-------------------------------
-    if not q.empty():
-      message = q.get()
-      m = message.split('_')
-      if m[0] == "steer":
-        steer_manual = float(m[1])
-        is_openpilot_engaged = False
-      elif m[0] == "throttle":
-        throttle_manual = float(m[1])
-        is_openpilot_engaged = False
-      elif m[0] == "brake":
-        brake_manual = float(m[1])
-        is_openpilot_engaged = False
-      elif m[0] == "reverse":
-        cruise_button = CruiseButtons.CANCEL
-        is_openpilot_engaged = False
-      elif m[0] == "cruise":
-        if m[1] == "down":
-          cruise_button = CruiseButtons.DECEL_SET
-          is_openpilot_engaged = True
-        elif m[1] == "up":
-          cruise_button = CruiseButtons.RES_ACCEL
-          is_openpilot_engaged = True
-        elif m[1] == "cancel":
+    old_steer = old_brake = old_throttle = 0
+    throttle_manual_multiplier = 0.7  # keyboard signal is always 1
+    brake_manual_multiplier = 0.7  # keyboard signal is always 1
+    steer_manual_multiplier = 45 * STEER_RATIO  # keyboard signal is always 1
+
+    while True:
+      # 1. Read the throttle, steer and brake from op or manual controls
+      # 2. Set instructions in Carla
+      # 3. Send current carstate to op via can
+
+      cruise_button = 0
+      throttle_out = steer_out = brake_out = 0.0
+      throttle_op = steer_op = brake_op = 0
+      throttle_manual = steer_manual = brake_manual = 0.0
+
+      # --------------Step 1-------------------------------
+      if not q.empty():
+        message = q.get()
+        m = message.split('_')
+        if m[0] == "steer":
+          steer_manual = float(m[1])
+          is_openpilot_engaged = False
+        elif m[0] == "throttle":
+          throttle_manual = float(m[1])
+          is_openpilot_engaged = False
+        elif m[0] == "brake":
+          brake_manual = float(m[1])
+          is_openpilot_engaged = False
+        elif m[0] == "reverse":
           cruise_button = CruiseButtons.CANCEL
           is_openpilot_engaged = False
-      elif m[0] == "ignition":
-        vehicle_state.ignition = not vehicle_state.ignition
-      elif m[0] == "quit":
-        break
+        elif m[0] == "cruise":
+          if m[1] == "down":
+            cruise_button = CruiseButtons.DECEL_SET
+            is_openpilot_engaged = True
+          elif m[1] == "up":
+            cruise_button = CruiseButtons.RES_ACCEL
+            is_openpilot_engaged = True
+          elif m[1] == "cancel":
+            cruise_button = CruiseButtons.CANCEL
+            is_openpilot_engaged = False
+        elif m[0] == "ignition":
+          vehicle_state.ignition = not vehicle_state.ignition
+        elif m[0] == "quit":
+          break
 
-      throttle_out = throttle_manual * throttle_manual_multiplier
-      steer_out = steer_manual * steer_manual_multiplier
-      brake_out = brake_manual * brake_manual_multiplier
+        throttle_out = throttle_manual * throttle_manual_multiplier
+        steer_out = steer_manual * steer_manual_multiplier
+        brake_out = brake_manual * brake_manual_multiplier
 
-      old_steer = steer_out
-      old_throttle = throttle_out
-      old_brake = brake_out
+        old_steer = steer_out
+        old_throttle = throttle_out
+        old_brake = brake_out
 
-    if is_openpilot_engaged:
-      sm.update(0)
+      if is_openpilot_engaged:
+        sm.update(0)
 
-      # TODO gas and brake is deprecated
-      throttle_op = clip(sm['carControl'].actuators.accel / 1.6, 0.0, 1.0)
-      brake_op = clip(-sm['carControl'].actuators.accel / 4.0, 0.0, 1.0)
-      steer_op = sm['carControl'].actuators.steeringAngleDeg
+        # TODO gas and brake is deprecated
+        throttle_op = clip(sm['carControl'].actuators.accel / 1.6, 0.0, 1.0)
+        brake_op = clip(-sm['carControl'].actuators.accel / 4.0, 0.0, 1.0)
+        steer_op = sm['carControl'].actuators.steeringAngleDeg
 
-      throttle_out = throttle_op
-      steer_out = steer_op
-      brake_out = brake_op
+        throttle_out = throttle_op
+        steer_out = steer_op
+        brake_out = brake_op
 
-      steer_out = steer_rate_limit(old_steer, steer_out)
-      old_steer = steer_out
+        steer_out = steer_rate_limit(old_steer, steer_out)
+        old_steer = steer_out
 
-    else:
-      if throttle_out == 0 and old_throttle > 0:
-        if throttle_ease_out_counter > 0:
-          throttle_out = old_throttle
-          throttle_ease_out_counter += -1
-        else:
-          throttle_ease_out_counter = REPEAT_COUNTER
-          old_throttle = 0
+      else:
+        if throttle_out == 0 and old_throttle > 0:
+          if throttle_ease_out_counter > 0:
+            throttle_out = old_throttle
+            throttle_ease_out_counter += -1
+          else:
+            throttle_ease_out_counter = REPEAT_COUNTER
+            old_throttle = 0
 
-      if brake_out == 0 and old_brake > 0:
-        if brake_ease_out_counter > 0:
-          brake_out = old_brake
-          brake_ease_out_counter += -1
-        else:
-          brake_ease_out_counter = REPEAT_COUNTER
-          old_brake = 0
+        if brake_out == 0 and old_brake > 0:
+          if brake_ease_out_counter > 0:
+            brake_out = old_brake
+            brake_ease_out_counter += -1
+          else:
+            brake_ease_out_counter = REPEAT_COUNTER
+            old_brake = 0
 
-      if steer_out == 0 and old_steer != 0:
-        if steer_ease_out_counter > 0:
-          steer_out = old_steer
-          steer_ease_out_counter += -1
-        else:
-          steer_ease_out_counter = REPEAT_COUNTER
-          old_steer = 0
+        if steer_out == 0 and old_steer != 0:
+          if steer_ease_out_counter > 0:
+            steer_out = old_steer
+            steer_ease_out_counter += -1
+          else:
+            steer_ease_out_counter = REPEAT_COUNTER
+            old_steer = 0
 
-    # --------------Step 2-------------------------------
-    steer_carla = steer_out / (max_steer_angle * STEER_RATIO * -1)
+      # --------------Step 2-------------------------------
+      steer_carla = steer_out / (max_steer_angle * STEER_RATIO * -1)
 
-    steer_carla = np.clip(steer_carla, -1, 1)
-    steer_out = steer_carla * (max_steer_angle * STEER_RATIO * -1)
-    old_steer = steer_carla * (max_steer_angle * STEER_RATIO * -1)
+      steer_carla = np.clip(steer_carla, -1, 1)
+      steer_out = steer_carla * (max_steer_angle * STEER_RATIO * -1)
+      old_steer = steer_carla * (max_steer_angle * STEER_RATIO * -1)
 
-    # TODO
+      # TODO
 
-    # vc.throttle = throttle_out / 0.6
-    # vc.steer = steer_carla
-    # vc.brake = brake_out
-    # vehicle.apply_control(vc)
+      # vc.throttle = throttle_out / 0.6
+      # vc.steer = steer_carla
+      # vc.brake = brake_out
+      # vehicle.apply_control(vc)
 
-    vc_throttle = throttle_out / 0.6
-    vc_steer = steer_carla
-    vc_brake = brake_out
+      vc_throttle = throttle_out
+      vc_steer = steer_carla
+      vc_brake = brake_out
 
-    # TODO
+      message = f'{vc_throttle} {vc_steer} {vc_brake}'
 
-    # --------------Step 3-------------------------------
-    # vel = vehicle.get_velocity()
-    # speed = math.sqrt(vel.x**2 + vel.y**2 + vel.z**2)  # in m/s
-    # vehicle_state.speed = speed
-    # vehicle_state.vel = vel
-    # TODO set speed
-    vehicle_state.speed = 0
-    vehicle_state.angle = steer_out
-    vehicle_state.cruise_button = cruise_button
-    vehicle_state.is_engaged = is_openpilot_engaged
+      sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
 
-    if rk.frame % PRINT_DECIMATION == 0:
-      # print("frame: ", "engaged:", is_openpilot_engaged, "; throttle: ", round(vc.throttle, 3), "; steer(c/deg): ", round(vc.steer, 3), round(steer_out, 3), "; brake: ", round(vc.brake, 3))
-      print("frame: ", "engaged:", is_openpilot_engaged, "; throttle: ", round(vc_throttle, 3), "; steer(c/deg): ", round(vc_steer, 3), round(steer_out, 3), "; brake: ", round(vc_brake, 3))
+      # TODO
 
-    # if rk.frame % 5 == 0:
-    #   world.tick()
+      # --------------Step 3-------------------------------
+      # vel = vehicle.get_velocity()
+      # speed = math.sqrt(vel.x**2 + vel.y**2 + vel.z**2)  # in m/s
+      # vehicle_state.speed = speed
+      # vehicle_state.vel = vel
+      # TODO set speed
+      vehicle_state.speed = throttle_out # replace if imu exists
+      vehicle_state.angle = steer_out
+      vehicle_state.cruise_button = cruise_button
+      vehicle_state.is_engaged = is_openpilot_engaged
 
-    rk.keep_time()
+      if rk.frame % PRINT_DECIMATION == 0:
+        # print("frame: ", "engaged:", is_openpilot_engaged, "; throttle: ", round(vc.throttle, 3), "; steer(c/deg): ", round(vc.steer, 3), round(steer_out, 3), "; brake: ", round(vc.brake, 3))
+        print("frame: ", "engaged:", is_openpilot_engaged, "; throttle: ", round(vc_throttle, 3), "; steer(c/deg): ", round(vc_steer, 3), round(steer_out, 3), "; brake: ", round(vc_brake, 3))
 
-  # Clean up resources in the opposite order they were created.
-  exit_event.set()
-  for t in reversed(threads):
-    t.join()
-  # gps.destroy()
-  # imu.destroy()
-  # camera.destroy()
-  # vehicle.destroy()
+      # if rk.frame % 5 == 0:
+      #   world.tick()
+
+      rk.keep_time()
+
+    # Clean up resources in the opposite order they were created.
+    exit_event.set()
+    for t in reversed(threads):
+      t.join()
+    # gps.destroy()
+    # imu.destroy()
+    # camera.destroy()
+    # vehicle.destroy()
 
 
 def bridge_keep_alive(q: Any):
